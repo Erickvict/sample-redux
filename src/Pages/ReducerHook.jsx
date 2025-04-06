07c9cb8d39007f3813c037c49e3ca52e7f127d84
Erickvict <appuerick4@gmail.com>
2025-04-06 17:58:53 +0530

    added

--- src/Pages/ReducerHook.jsx
+++ src/Pages/ReducerHook.jsx
@@ -28,13 +28,13 @@ const ReducerHook = () => {
     //in reducer function we have 2 arg...(state,action)
     //what we passing into the dispatch we get in the action parameter
 
-
+  //increment function
     const increment=()=>
     {
       dispatch({type:"increment"})
     }
 
-
+//decrement function
     const decrement=()=>
       {
         dispatch({type:"decrement"})
